5cf5f2a46f7fad5c047d20b84c43fe4cd3ba9453
Lawani Osaro <osarolawani983@gmail.com>
2023-06-02 15:29:08 +0100

    resolved some issues

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
             </div>
             <div>
                 <h2 className='font-bold text-[24px] mb-2'>Here is a little about me</h2>
-                <p className='max-w-[470px]'>Hi, I'm Osaro, a passionate frontend web developer with expertise in HTML, CSS, JavaScript, React, and Firebase.
+                <p className='max-w-[470px]'>Hi, I'm Osaro, a passionate frontend web developer with expertise in HTML, CSS, JavaScript, Git, Github, React, and Firebase.
                 I have a strong foundation in web development principles and a keen eye for design. My goal is to create visually appealing,
                 user-friendly, and responsive websites that provide a seamless experience across devices.</p>
                 <Link to="projects" smooth={true} duration={500} className='mt-4 border-2 dark:border-white border-black px-4 py-2 hover:bg-black flex gap-2 items-center hover:text-white transition-all dark:hover:bg-white dark:hover:text-black w-[160px]'>See Projects <BiCodeAlt/></Link>
